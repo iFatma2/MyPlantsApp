a6cdd5add8be296dc0d2bc43a73cd9ec2aaa115a
iFatma2 <fatma.m.q@outlook.sa>
2024-10-20 12:05:43 +0300

    Initial Commit

---
+++ MyPlantsApp/MyPlantsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlantsAppApp.swift
+//  MyPlantsApp
+//
+//  Created by Fatimah Alqarni on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct MyPlantsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
